5aba6cedbd53ac8385e9dae2c8bc8ed5bc7d798d
Wize Flux <fear.ru@gmail.com>
2012-03-27 13:47:57 +0800

    Booking create success fash message

--- app/controllers/estate_bookings_controller.rb
+++ app/controllers/estate_bookings_controller.rb
@@ -1,29 +1,31 @@
 class EstateBookingsController < BookingsController
-  include Traits::ParentResource
+    include Traits::ParentResource
 
-  before_filter :find_parent_resource
-  before_filter :new_resource, :only => %w[new create index]
-  before_filter :find_resources, :only => %w[index]
-  before_filter :find_resource, :only => %w[delete destroy update show edit]
+    before_filter :find_parent_resource
+    before_filter :new_resource, :only => %w[new create index]
+    before_filter :find_resources, :only => %w[index]
+    before_filter :find_resource, :only => %w[delete destroy update show edit]
   
-  def resource_conditions
-    {:estate_id => parent_resource_param}
-  end
+    def resource_conditions
+        {:estate_id => parent_resource_param}
+    end
   
-  def resource_attributes
-    (super || {}).merge!(
-        {
+    def resource_attributes
+        (super || {}).merge!({
             :estate => @estate,
             :agency => @estate.agency,
-        }
-    )
-  end
+        })
+    end
   
-  def post_create_location
-    estate_booking_url(@estate, @booking)
-  end
+    def post_create_location
+        estate_booking_url(@estate, @booking)
+    end
   
-  def post_update_location
-    estate_booking_url(@estate, @booking)
-  end
+    def create_success_flash
+        t 'notices.booking.success'
+    end
+  
+    def post_update_location
+        estate_booking_url(@estate, @booking)
+    end
 end
--- app/controllers/traits/actions/create.rb
+++ app/controllers/traits/actions/create.rb
@@ -1,33 +1,37 @@
 module Traits::Actions
-  module Create
-    def create
-      if resource_create
-        resource_create_success
-      else
-        resource_create_failure
-      end
-    end
-    
-    protected
+    module Create
+        def create
+            if resource_create
+                resource_create_success
+            else
+                resource_create_failure
+            end
+        end
+
+        protected
   
-    def new_resource
-      self.current_resource = resource_scope.new resource_attributes
+        def new_resource
+            self.current_resource = resource_scope.new resource_attributes
+        end
+
+        def post_create_location
+            current_resource
+        end
+
+        def resource_create
+            self.current_resource.save
+        end
+
+        def resource_create_success
+            redirect_to post_create_location, :flash => {:success => create_success_flash}
+        end
+
+        def create_success_flash
+            'Resource was succesfully created'
+        end
+
+        def resource_create_failure
+            render :new
+        end 
     end
-
-    def post_create_location
-      current_resource
-    end 
-
-    def resource_create
-      self.current_resource.save
-    end
-
-    def resource_create_success
-      redirect_to post_create_location
-    end
-
-    def resource_create_failure
-      render :new
-    end 
-  end
 end
