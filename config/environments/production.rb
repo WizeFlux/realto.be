359e078b6d4ad55af7cea5f864d1c5a76f2158b7
Wize Flux <fear.ru@gmail.com>
2012-04-21 14:12:31 +0800

    move to 3.2.3 rails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ RealtoBe::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
@@ -31,7 +31,7 @@ RealtoBe::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  config.log_level = :error
+  config.log_level = :info
 
   config.log_tags = [ :subdomain ]
 
